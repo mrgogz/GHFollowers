82f66435538e572215b86c8e8e37efecfad1ea6d
mrgogz <92844107+mrgogz@users.noreply.github.com>
2024-07-20 15:43:23 +0300

    Update

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Oğuzcan Beşerikli on 20.07.2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
