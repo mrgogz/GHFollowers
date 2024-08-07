1b5b2efdeeafe78df8f9dfd996bf577b9b794585
mrgogz <92844107+mrgogz@users.noreply.github.com>
2024-08-07 14:17:39 +0300

    Update

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Oğuzcan Beşerikli on 7.08.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mapping.and.ellipse"
+}
